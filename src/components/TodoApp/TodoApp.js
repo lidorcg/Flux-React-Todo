682c4fd52c2352f6e3df733e6c3e865c9a90d266
lidorcg <lidorcg@gmail.com>
2015-12-13 19:36:08 +0200

    - cleaned the project and we are ready to go

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class TodoApp extends Component {
   render() {
     return (
       <h1>Hello, world.</h1>
